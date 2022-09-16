6c9d9e3bcc7b2d84ff1f83ddec248796d93c2aaf
Ankit <ap644531@gmail.com>
2022-09-16 08:37:56 +0530

    Leetcode

--- DSA by Love babbar/L6/Dec_to_binary.cpp
+++ DSA by Love babbar/L6/Dec_to_binary.cpp
@@ -11,13 +11,12 @@ int main()
     
     while(n!=0)
     {
-        //   int bit = n & 1;
-        //   ans = (bit*pow(10,i)+ans);
-        //   n= n>>1;
-        //   i++;
-        cout << (n & 1);
-        n = n>>1;
+          int bit = n & 1;
+          ans = (bit*pow(10,i)+ans);
+          n = n>>1;
+          i++;
     }
-    // cout<< ans<<endl;
+    
+    cout<< ans<<endl;
 
 };
