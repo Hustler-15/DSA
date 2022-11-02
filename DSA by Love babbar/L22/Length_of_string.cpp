7ca995aceb384331bdd00aeec1a5f4f5f200fba4
Ankit <ap644531@gmail.com>
2022-11-02 06:33:34 +0530

    Char Arrays

---
+++ DSA by Love babbar/L22/Length_of_string.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int getLength(char name[])
+{
+    int count = 0;
+    for(int i = 0;name[i]!='\0';i++)
+    {
+        count++;
+    } 
+
+    return count;
+}
