d9adb0f6c7a95883954ff947b128bd9ecdda750d
Ankit <ap644531@gmail.com>
2023-01-03 21:28:33 +0530

    Floyd detection algorithm

--- DSA by Love babbar/L47/removeLoopInLL.cpp
+++ DSA by Love babbar/L47/removeLoopInLL.cpp
@@ -49,3 +49,46 @@ Node* floydDetectLoop(Node* head)
 
 
 }
+
+Node* getStartingNode(Node* head)
+{
+    if(head == NULL)
+    {
+        return NULL;
+    }
+
+    Node* intersection = floydDetectLoop(head);
+    if(intersection == NULL)
+    {
+        return NULL;
+    }
+    Node* slow = head;
+    while(slow!=intersection)
+    {
+        slow = slow->next;
+        intersection = intersection->next;
+    }
+
+    return slow;
+}
+
+Node* removeLoop(Node* head)
+{
+    if(head == NULL)
+    {
+        return NULL;
+    }
+
+    Node* startOfLoop = getStartingNode(head);
+    if(startOfLoop == NULL)
+    {
+        return head;
+    }
+    Node* temp = startOfLoop;
+    while(temp->next != startOfLoop)
+    {
+        temp = temp->next;
+    }
+    temp->next = NULL;
+    return head;
+}
--- DSA by Love babbar/L47/startNodeOfLoopInLL.cpp
+++ DSA by Love babbar/L47/startNodeOfLoopInLL.cpp
@@ -49,3 +49,21 @@ Node* floydDetectLoop(Node* head)
 
 
 }
+
+Node* getStartingNode(Node* head)
+{
+    if(head == NULL)
+    {
+        return NULL;
+    }
+
+    Node* intersection = floydDetectLoop(head);
+    Node* slow = head;
+    while(slow!=intersection)
+    {
+        slow = slow->next;
+        intersection = intersection->next;
+    }
+
+    return slow;
+}
