60920cbdb33e75fe17146e1402c72ae46ef6d46e
Ankit <ap644531@gmail.com>
2023-01-04 19:39:02 +0530

    Sort LL

---
+++ DSA by Love babbar/L49/sort0,1,2InLL.cpp
@@ -0,0 +1,77 @@
+#include<iostream>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* next;
+
+    Node(int d)
+    {
+        this->data = d;
+        this->next = NULL; 
+    }
+
+    ~Node()
+    {
+        int value = this->data;
+        if(this->next!=NULL)
+        {
+            delete next;
+            next = NULL;
+        }
+        cout<<"Memory is free for data and value is "<<value<<endl;
+    }
+};
+
+Node* sortList(Node* head)
+{
+    int zeroCount = 0;
+    int oneCount = 0;
+    int twoCount = 0;
+
+    Node* temp = head;
+    while(temp!=NULL)
+    {
+        if(temp->data == 0)
+        {
+            zeroCount++;
+        }
+        else if(temp->data == 1)
+        {
+            oneCount++;
+        }
+        else if(temp->data == 2)
+        {
+            twoCount++;
+        }
+        temp = temp->next;
+    }
+
+    temp = head;
+    while(temp!=NULL)
+    {
+        if(zeroCount != 0)
+        {
+            temp->data = 0;
+            zeroCount--;
+            
+        }
+        else if(oneCount != 0)
+        {
+            temp->data = 1;
+            oneCount--;
+        }
+        else if(twoCount != 0)
+        {
+            temp->data = 2;
+            twoCount--;
+        }
+
+        temp = temp->next;
+    }
+
+    return head;
+}
+
+//https://www.codingninjas.com/codestudio/problems/sort-linked-list-of-0s-1s-2s_1071937?leftPanelTab=0
