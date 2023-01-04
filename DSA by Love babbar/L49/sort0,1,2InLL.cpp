6bc6c40e5d6170b726c905e014774823fd72065d
Ankit <ap644531@gmail.com>
2023-01-04 20:39:46 +0530

    Merge sort LL

--- DSA by Love babbar/L49/sort0,1,2InLL.cpp
+++ DSA by Love babbar/L49/sort0,1,2InLL.cpp
@@ -24,6 +24,8 @@ class Node{
     }
 };
 
+//<--------------------------------------------Approach 1--------------------------------------------->
+
 Node* sortList(Node* head)
 {
     int zeroCount = 0;
@@ -73,5 +75,63 @@ Node* sortList(Node* head)
 
     return head;
 }
+
+//<-------------------------------------------Approach 2--------------------------------------------->
+void insertAtTail(Node* &tail, Node* curr)
+{
+    tail->next = curr;
+    tail = curr;
+}
+Node* sortList1(Node* head)
+{
+    Node* zeroHead = new Node(-1);
+    Node* zeroTail = zeroHead;
+    Node* oneHead = new Node(-1);
+    Node* oneTail = oneHead;
+    Node* twoHead = new Node(-1);
+    Node* twoTail = twoHead;
+
+    //Creating seperate list
+    Node* curr = head;
+    while(curr!=NULL)
+    {
+        int value = curr->data;
+        if(value == 0)
+        {
+            insertAtTail(zeroTail,curr);
+        }
+        else if (value == 1)
+        {
+            insertAtTail(oneTail,curr);
+        }
+        else if (value == 2)
+        {
+            insertAtTail(twoTail,curr);
+        }
+        curr = curr->next;
+    }
+
+    //Merging seperate list
+    if(oneHead->next!=NULL)
+    {
+        zeroTail->next = oneHead->next;
+    }
+    else{
+        zeroTail->next = twoHead->next;
+    }
+    oneTail->next = twoHead->next;
+    twoTail->next = NULL;
+
+    //Setup Head
+    head = zeroHead->next;
+
+    //delete dummy
+    delete zeroHead;
+    delete oneHead;
+    delete twoHead;
+
+    return head;
+}
+
 
 //https://www.codingninjas.com/codestudio/problems/sort-linked-list-of-0s-1s-2s_1071937?leftPanelTab=0
