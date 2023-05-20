fcd94967512b71376376c643983aa434a86149e7
Ankit <ap644531@gmail.com>
2023-05-20 18:20:35 +0530

    BST

--- DSA by Love babbar/L69/bstIntro.cpp
+++ DSA by Love babbar/L69/bstIntro.cpp
@@ -11,43 +11,105 @@ public:
     {
         this->data = data;
         this->left = NULL;
-        this->right = NULL;    
+        this->right = NULL;
     }
 };
 
-Node* insertIntoBst(Node* &root, int data)
+Node *insertIntoBst(Node *&root, int data)
 {
-    //Base case
-    if(root == NULL)
+    // Base case
+    if (root == NULL)
     {
         root = new Node(data);
         return root;
     }
 
-    if(data>root->data)
+    if (data > root->data)
     {
-        root->right = insertIntoBst(root->right,data);
+        root->right = insertIntoBst(root->right, data);
     }
-    else{
-        root->left = insertIntoBst(root->left,data);
+    else
+    {
+        root->left = insertIntoBst(root->left, data);
     }
     return root;
-
 }
 
-void takeInput(Node* &root){
+void takeInput(Node *&root)
+{
     int data;
-    cin>>data;
+    cin >> data;
 
-    while(data!=1)
+    while (data != 1)
     {
         insertIntoBst(root, data);
-        cin>>data;
+        cin >> data;
+    }
+}
+Node *minVal(Node *root)
+{
+    Node *temp = root;
+    while (temp != NULL)
+    {
+        temp = temp->left;
+    }
+    return temp;
+}
+
+Node *deleteFromBST(Node *root, int val)
+{
+    if (root == NULL)
+        return root;
+
+    if (root->data == val)
+    {
+        // 0 child
+        if (root->left == NULL & root->right == NULL)
+        {
+            delete root;
+            return NULL;
+        }
+        // 1 child
+
+        // left child
+        if (root->left != NULL && root->right == NULL)
+        {
+            Node *temp = root->left;
+            delete root;
+            return temp;
+        }
+        // right child
+        if (root->left == NULL && root->right != NULL)
+        {
+            Node *temp = root->right;
+            delete root;
+            return temp;
+        }
+        // 2 child
+        if (root->left != NULL & root->right != NULL)
+        {
+            int mini = minVal(root->right)->data;
+            root->data = mini;
+            root->right = deleteFromBST(root->right, mini);
+            return root;
+        }
+    }
+
+    else if (root->data > val)
+    {
+        root->left = deleteFromBST(root->left, val);
+        return root;
+    }
+    else
+    {
+        root->right = deleteFromBST(root->right, val);
+        return root;
     }
 }
 
-void main(){
-    Node* root = NULL;
-    cout<<"Enter data to make bst"<<endl;
+void main()
+{
+    Node *root = NULL;
+    cout << "Enter data to make bst" << endl;
     takeInput(root);
 }
