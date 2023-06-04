689654251bd8d25862c473f9e99e0166c3688cdf
Ankit <ap644531@gmail.com>
2023-06-04 16:43:50 +0530

    BST

---
+++ DSA by Love babbar/L71/2sumInBST.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+class BinaryTreeNode
+{
+public:
+    int data;
+    BinaryTreeNode *left;
+    BinaryTreeNode *right;
+
+    BinaryTreeNode(int data)
+    {
+        this->data = data;
+        left = NULL;
+        right = NULL;
+    }
+};
+void inorder(BinaryTreeNode<int> *root, vector<int> &inorderVal)
+{
+    if (root == NULL)
+        return;
+
+    inorder(root->left, inorderVal);
+    inorderVal.push_back(root->data);
+    inorder(root->right, inorderVal);
+}
+bool twoSumInBST(BinaryTreeNode<int> *root, int target)
+{
+    vector<int> inorderVal;
+    inorder(root, inorderVal);
+    int s = 0;
+    int e = inorderVal.size() - 1;
+    while (s < e)
+    {
+        int sum = inorderVal[s] + inorderVal[e];
+        if (sum == target)
+        {
+            return true;
+        }
+        else if (sum > target)
+            e--;
+        else
+            s++;
+    }
+    return false;
+}
+// https://www.codingninjas.com/codestudio/problems/two-sum-in-a-bst_1062631?leftPanelTab=1
