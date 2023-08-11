760a37a52cd6fd62e818179357e6f3a748d014b7
Ankit <ap644531@gmail.com>
2023-08-11 09:18:51 +0530

    Heaps

---
+++ DSA by Love babbar/L76/kthLargestSubarraySum.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <algorithm>
+int getKthLargest(vector<int> &arr, int k)
+{
+	vector<int> sumStore;
+	int n = arr.size();
+	// step1: sum of all sub array
+	for (int i = 0; i < n; i++)
+	{
+		int sum = 0;
+		for (int j = i; j < n; j++)
+		{
+			sum += arr[j];
+			sumStore.push_back(sum);
+		}
+	}
+	// step2:sort the answer array
+	sort(sumStore.begin(), sumStore.end());
+
+	return sumStore[sumStore.size() - k];
+}
+
+// Approach 2
+int getKthLargest(vector<int> &arr, int k)
+{
+	priority_queue<int, vector<int>, greater<int>> mini;
+	int n = arr.size();
+
+	for (int i = 0; i < n; i++)
+	{
+		int sum = 0;
+		for (int j = i; j < n; j++)
+		{
+			sum += arr[j];
+			if (mini.size() < k)
+			{
+				mini.push(sum);
+			}
+			else
+			{
+				if (sum > mini.top())
+				{
+					mini.pop();
+					mini.push(sum);
+				}
+			}
+		}
+	}
+
+	return mini.top();
+}
+
+// https://www.codingninjas.com/studio/problems/k-th-largest-sum-contiguous-subarray_920398?leftPanelTab=1&utm_source=youtube&utm_medium=affiliate&utm_campaign=Lovebabbar
