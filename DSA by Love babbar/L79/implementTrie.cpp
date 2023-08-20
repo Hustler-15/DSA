a6bef5e5fef4f52a5242e2ece364e4261c1896b5
Ankit <ap644531@gmail.com>
2023-08-20 10:17:49 +0530

    Trie

---
+++ DSA by Love babbar/L79/implementTrie.cpp
@@ -0,0 +1,125 @@
+#include <iostream>
+using namespace std;
+class TrieNode
+{
+public:
+    char data;
+    TrieNode *children[26];
+    bool isTerminal;
+
+    TrieNode(char ch)
+    {
+        data = ch;
+        for (int i = 0; i < 26; i++)
+        {
+            children[i] = NULL;
+        }
+        isTerminal = false;
+    }
+};
+
+class Trie
+{
+    TrieNode *root;
+
+public:
+    /** Initialize your data structure here. */
+    Trie()
+    {
+        root = new TrieNode('\0');
+    }
+
+    void insertUtil(TrieNode *root, string word)
+    {
+        // base case
+        if (word.length() == 0)
+        {
+            root->isTerminal = true;
+            return;
+        }
+
+        // assumption word will be in caps
+        int index = word[0] - 'a';
+        TrieNode *child;
+
+        // present
+        if (root->children[index] != NULL)
+        {
+            child = root->children[index];
+        }
+        else
+        {
+            // absent
+            child = new TrieNode(word[0]);
+            root->children[index] = child;
+        }
+        return insertUtil(child, word.substr(1));
+    }
+
+    /** Inserts a word into the trie. */
+    void insert(string word)
+    {
+        insertUtil(root, word);
+    }
+
+    bool searchUtil(TrieNode *root, string word)
+    {
+        // base case
+        if (word.length() == 0)
+        {
+            return root->isTerminal;
+        }
+        // assumption word will be in caps
+        int index = word[0] - 'a';
+        TrieNode *child;
+
+        // present
+        if (root->children[index] != NULL)
+        {
+            child = root->children[index];
+        }
+        else
+        {
+            // absent
+            return false;
+        }
+        return searchUtil(child, word.substr(1));
+    }
+
+    /** Returns if the word is in the trie. */
+    bool search(string word)
+    {
+        return searchUtil(root, word);
+    }
+
+    bool prefixUtil(TrieNode *root, string word)
+    {
+        // base case
+        if (word.length() == 0)
+        {
+            return true;
+        }
+        // assumption word will be in caps
+        int index = word[0] - 'a';
+        TrieNode *child;
+
+        // present
+        if (root->children[index] != NULL)
+        {
+            child = root->children[index];
+        }
+        else
+        {
+            // absent
+            return false;
+        }
+        return prefixUtil(child, word.substr(1));
+    }
+    /** Returns if there is any word in the trie that starts with the given prefix. */
+    bool startsWith(string prefix)
+    {
+        return prefixUtil(root, prefix);
+    }
+};
+
+// https://www.codingninjas.com/studio/problems/implement-trie_631356?topList=love-babbar-dsa-sheet-problems&leftPanelTab=1&utm_source=youtube&utm_medium=affiliate&utm_campaign=Lovebabbar
